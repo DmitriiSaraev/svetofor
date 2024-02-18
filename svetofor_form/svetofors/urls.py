dd724362a82887f68515c835c82af038604e7c4e
Pc <saraevdmitrii@gmail.com>
2024-02-18 16:18:59 +0500

    three form

--- svetofor_form/svetofors/urls.py
+++ svetofor_form/svetofors/urls.py
@@ -7,5 +7,9 @@ app_name = "svetofors"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("empty_model/", views.empty_model, name="empty_model"),
     path('svetofor/<int:svetofor_id>/up_svetofor/', views.up_svetofor, name='up_svetofor',),
+    path('', views.up_svetofor_empty, name='up_svetofor_empty',),
 ]
+
+
--- svetofor_form/svetofors/views.py
+++ svetofor_form/svetofors/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 from svetofors.forms import SetupPhaseSvetofor, SetupPhaseSvetoforModel
@@ -6,14 +7,58 @@ from svetofors.models import Svetofor
 
 def index(request):
     svetofors = Svetofor.objects.all()
+    form = SetupPhaseSvetoforModel
 
-    form = SetupPhaseSvetoforModel()
+    if request.method == 'POST':
+        form = SetupPhaseSvetoforModel(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        form.save()
+        return render(request, 'index.html', context={'svetofors': svetofors,
+                                                      'form': form})
 
-    return render(request, 'index.html', context={'svetofors': svetofors})
+
+    return render(request, 'index.html', context={'svetofors': svetofors,
+                                                  'form': form})
+
 
 def up_svetofor(request, svetofor_id):
-    """Добавление товара в корзину"""
+    if request.method == 'POST':
+        ip_address = request.POST.get(
+            'ip_address')  # получить значение поля ip_address
+        local = request.POST.get('local')  # получить значение поля local
+        protocol = request.POST.get(
+            'protocol')  # получить значение поля protocol
+        print(ip_address, local, protocol)
+
+        # Здесь вы можете использовать полученные данные, сохранить их в базу данных или выполнять другие операции
 
     svetofor = Svetofor.objects.get(pk=svetofor_id)
     print(svetofor_id)
-    return redirect("svetofors:index", )
+    return redirect("svetofors:index")
+
+
+def empty_model(request):
+    svetofors = Svetofor()
+
+    forms = []
+
+    for i in range(3):
+        forms.append(SetupPhaseSvetoforModel(prefix=str(i)))
+    # forms = [SetupPhaseSvetoforModel(prefix=str(i)) for i in range(3)]
+
+    return render(request, 'empty_model.html', {'forms': forms})
+
+
+
+
+def up_svetofor_empty(request):
+    if request.method == 'POST':
+        form = SetupPhaseSvetoforModel(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        form.save()
+        return HttpResponseRedirect('')  # Перенаправление на страницу успеха
+    else:
+        return HttpResponseRedirect('')
+
